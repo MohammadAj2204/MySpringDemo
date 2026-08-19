bb60b14e532258799936a0e2eca4f88e106cba08
MohammadAj2204 <patanajam2204@gmail.com>
2026-08-19 18:35:04 +0530

    crud all done

--- src/main/java/com/example/demotest/Controller/Controller.java
+++ src/main/java/com/example/demotest/Controller/Controller.java
@@ -1,34 +1,46 @@
 package com.example.demotest.Controller;
-
 import com.example.demotest.Model.Student;
 import com.example.demotest.Service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
 @RestController
 public class Controller {
 
     private final StudentService studentService;
-    public Controller(StudentService studentService){
-        this.studentService=studentService;
+
+    public Controller(StudentService studentService) {
+        this.studentService = studentService;
     }
+
     @GetMapping("/hello")
-    public String hello(){
-     return " Hello world";
+    public String hello() {
+        return " Hello world";
     }
+
     @PostMapping("/addstudent")
-    public Student addstud(@RequestBody Student student ) {
+    public Student addstud(@RequestBody Student student) {
         return studentService.savestudent(student);
     }
+
     @GetMapping("/getall")
-    public List<Student> getall(){
+    public List<Student> getall() {
         return studentService.getall();
     }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable Integer id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/student/{id}")
+    public Student updatestudent(@PathVariable Integer id, @RequestBody Student student){
+        return studentService.upadtestudent(id,student);
+}
+@DeleteMapping("/student/{id}")
+    public String deletestudent(@PathVariable Integer id ){
+        studentService.deleteStudent(id);
+        return "Student Deleted Successfully "+ id;
+}
 
 
 }
--- src/main/java/com/example/demotest/Service/StudentService.java
+++ src/main/java/com/example/demotest/Service/StudentService.java
@@ -4,13 +4,15 @@ import com.example.demotest.Model.Student;
 import com.example.demotest.StudentRepo.StudentRepoo;
 import org.springframework.stereotype.Service;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
 
-    private final StudentRepoo studentRepoo;
+    private final StudentRepoo studentRepoo ;
     public StudentService( StudentRepoo studentRepoo){
         this.studentRepoo=studentRepoo;
     }
@@ -20,8 +22,25 @@ public class StudentService {
     }
 public Student savestudent(Student student){
         return studentRepoo.save(student);
-
 }
 
+    public Student getStudentById(Integer id) {
+        Optional<Student> student = studentRepoo.findById(id);
+        return student.orElse(null);
+
+}
+public Student upadtestudent(int id ,Student updatestudent){
+        Student student =studentRepoo.findById(id).orElse(null);
+        if( student== null){
+            return null;
+        }
+        student.setAge(updatestudent.getAge());
+        student.setName(updatestudent.getName());
+        return studentRepoo.save(student);
+}
+
+    public void deleteStudent(Integer id) {
+        studentRepoo.deleteById(id);
+    }
 
 }
